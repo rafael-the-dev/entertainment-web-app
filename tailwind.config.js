8ffadb8a3d78f8971e348ea78b08ab4848797449
Rafael Tivane <luislangabusiness@gmail.com>
2022-03-24 12:40:31 +0200

    adding colors, and screen break points

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          600: '#FC4747'
+        },
+        blue: {
+          500: '#FC4747',
+          600: '#161D2F',
+          700: '#10141E'
+        },
+      }
+    },
+    screens: {
+      'sm': '600px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '900px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
