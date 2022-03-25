8b1ca085dc2212c3eb354da73cbb00cf2b73f711
Rafael Tivane <luislangabusiness@gmail.com>
2022-03-25 06:42:32 +0200

    customizing card content for trending section

--- src/shared/components/MovieCard/index.jsx
+++ src/shared/components/MovieCard/index.jsx
@@ -1,8 +1,12 @@
+import { Typography } from "@mui/material";
 import classNames from "classnames";
+import { useStyles } from "./styles";
 
 
 
 const MovieCard = () => {
+    const classes = useStyles();
+
     return (
         <article className={classNames(`relative`)}>
             <div>
@@ -12,12 +16,17 @@ const MovieCard = () => {
                     src=""
                 />
             </div>
-            <div className={classNames(`absolute bottom-[-20px] left-[rem]`)}>
+            <div className={classNames(classes.cardContentTrending, `absolute bottom-[-20px] left-[rem]`)}>
                 <p>
                     <span>2019</span>
                     <span>2019</span>
                     <span className={classNames(`uppercase`)}>pg</span>
                 </p>
+                <Typography 
+                    component="h3"
+                    variant="h6">
+                    Beyond Earth
+                </Typography>
             </div>
         </article>
     );
---
+++ src/shared/components/MovieCard/styles.js
@@ -0,0 +1,7 @@
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles(theme => ({
+    cardContentTrending: {
+        backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, .2), rgba(0, 0, 0, .5))'
+    }
+}));
